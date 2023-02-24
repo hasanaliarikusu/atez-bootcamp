20cfc6bab0ff6f7057049595f58b3ff926fed65d
hasanaliarikusu <108197894+hasanaliarikusu@users.noreply.github.com>
2023-02-24 21:35:08 +0300

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,12 @@
+let number = prompt ("sayı giriniz")
+let factor =1;
+if(number==0){
+    console.log(`${number} faktöriyel 1`);
+} else if(number < 0){
+    console.log(`Pozif Değer giriniz`);
+}else{
+    for(let i=1; i<=number;i++){
+        factor=factor * i;
+    }
+    console.log(`${number} faktöriyel ${factor}`)
+}
