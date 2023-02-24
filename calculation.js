0b5f4fb8828486943af72599930a6fcf4e8c42c9
hasanaliarikusu <108197894+hasanaliarikusu@users.noreply.github.com>
2023-02-25 00:19:49 +0300

    Update calculation.js

--- calculation.js
+++ calculation.js
@@ -1,9 +1,9 @@
 function calcSomething (x,y) {
-    const x = 3;
-    const y = 4;
+    const z = 3;
+    const t = 4;
     
     return x*y*z*t;
     
 };
 
-console.log(calcSomething(z=3,t=5));
+console.log(calcSomething(x=3,y=5));
