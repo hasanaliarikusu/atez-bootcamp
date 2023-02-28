1f5aa9573b974fd23ccd5324950ea58b9d281554
hasanaliarikusu <108197894+hasanaliarikusu@users.noreply.github.com>
2023-02-28 22:43:17 +0300

    Add files via upload

---
+++ enums.ts
@@ -0,0 +1,11 @@
+enum CardinalDirections {
+  North,
+  East,
+  South,
+  West
+};
+            
+let currentDirection = CardinalDirections.North;
+
+// North is the first value so it logs '0'
+console.log(currentDirection);
